907dfa7c185b1004ea17b3246a571259fd90ebf9
Yuliya Karimova <yuliya.karimova@wn.media>
2023-10-14 23:16:23 +0300

    feat: mobile version

--- frontend/src/components/Content.vue
+++ frontend/src/components/Content.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="flex gap-12 items-center" :class="textAlign === 'right' && 'flex-row-reverse'">
-    <div class="w-1/2">
+  <div class="flex flex-col gap-6 sm:gap-12 items-center" :class="textAlign === 'right' ? 'sm:flex-row-reverse' : 'sm:flex-row'">
+    <div class="w-full sm:w-1/2">
       <div class="text-2xl uppercase font-bold pb-2">{{ title }}</div>
-      <div class="pb-6 font-light">{{ description }}</div>
+      <div class="font-light">{{ description }}</div>
     </div>
-    <div class="w-1/2">
+    <div class="w-full sm:w-1/2">
       <div
         class="aspect-w-1 aspect-h-1 rounded-2xl"
         :style="`background-image: url('${img}'); background-size: cover; background-position: center;`"
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <div class="flex items-center mx-auto max-w-screen-lg px-8 py-20">
-      <div class="w-2/3 uppercase text-5xl">
+    <div class="flex items-center mx-auto max-w-screen-lg px-8 py-12 sm:py-20">
+      <div class="w-full sm:w-2/3 uppercase text-3xl sm:text-5xl z-10">
         <div>Городская</div>
         <div>инфраструктура и</div>
-        <div class="font-bold text-8xl">здоровье</div>
+        <div class="font-bold text-6xl sm:text-8xl">здоровье</div>
       </div>
-      <div class="w-1/3">
+      <div class="absolute right-0 sm:static w-1/3 z-0">
         <div
           :style="`background: center / contain no-repeat url('./people/10.svg');`"
           class="aspect-w-2 aspect-h-3 -scale-x-100"
@@ -15,11 +15,13 @@
     </div>
 
     <div class="relative">
-      <div class="relative z-10 mx-auto max-w-screen-lg px-8 py-36">
-        <div class="text-4xl text-red-600 uppercase font-bold mb-10">
+      <div class="relative z-10 mx-auto max-w-screen-lg px-8 py-12 sm:py-36">
+        <div
+          class="text-2xl sm:text-4xl text-red-600 uppercase font-bold mb-10"
+        >
           Влияние городской инфраструктуры на здоровье
         </div>
-        <div class="text-2xl flex flex-col gap-10">
+        <div class="text-xl sm:text-2xl flex flex-col gap-10">
           <div>
             Добро пожаловать на наш веб-сайт, посвященный анализу влияния
             инфраструктуры города на здоровье горожан! Здесь мы изучаем
@@ -58,16 +60,22 @@
     </div>
 
     <div class="bg-cyan-950">
-      <div class="flex flex-col mx-auto max-w-screen-lg px-8 py-28 text-white">
-        <div class="flex flex-col gap-12 mb-24">
-          <div class="text-5xl font-bold">Приступить к анализу!</div>
-          <div class="flex gap-6">
-            <router-link to="/analysis"
+      <div
+        class="flex flex-col mx-auto max-w-screen-lg px-8 py-12 sm:py-28 text-white"
+      >
+        <div class="flex flex-col gap-8 sm:gap-12 sm:mb-24">
+          <div class="text-3xl sm:text-5xl font-bold">
+            Приступить к анализу!
+          </div>
+          <div class="flex gap-4 sm:gap-6 flex-col sm:flex-row">
+            <router-link
+              to="/analysis"
               class="rounded-sm h-8 bg-cyan-500 p-6 flex items-center justify-center text-lg font-medium"
             >
               Получить анализ города
             </router-link>
-            <router-link to="/compare"
+            <router-link
+              to="/compare"
               class="rounded-sm h-8 bg-white text-cyan-900 p-6 flex items-center justify-center text-lg font-medium"
             >
               Сравнить города
@@ -75,23 +83,25 @@
           </div>
         </div>
 
-        <div class="lg:w-1/3 mx-auto mb-8">
+        <div class="w-full sm:w-1/3 mx-auto sm:mb-8">
           <div
             :style="`background: center / contain no-repeat url('./people/3.svg');`"
             class="aspect-w-2 aspect-h-3"
           />
         </div>
 
-        <div class="flex gap-20 mb-36">
-          <div class="w-1/2 text-5xl font-bold">Аналитика – ключ к успеху!</div>
-          <div class="w-1/2 text-xl">
+        <div class="flex flex-col sm:flex-row gap-6 sm:gap-20 mb-12 sm:mb-36">
+          <div class="sm:w-1/2 text-5xl font-bold">
+            Аналитика – ключ к успеху!
+          </div>
+          <div class="sm:w-1/2 text-xl">
             Используя нашу платформу, вы сможете получить глубокий анализ
             городской среды и точные рекомендации по ее улучшению. Превратим
             наши города в оазисы здоровья и благополучия!
           </div>
         </div>
 
-        <div class="grid grid-cols-6 gap-6 mb-36">
+        <div class="grid grid-cols-3 sm:grid-cols-6 gap-6 mb-12 sm:mb-36">
           <div
             :style="`background: center / contain no-repeat url('./people/4.svg');`"
             class="aspect-w-2 aspect-h-3"
@@ -106,57 +116,57 @@
           />
           <div
             :style="`background: center / contain no-repeat url('./people/7.svg');`"
-            class="aspect-w-2 aspect-h-3"
+            class="aspect-w-2 aspect-h-3 hidden sm:block"
           />
           <div
             :style="`background: center / contain no-repeat url('./people/8.svg');`"
-            class="aspect-w-2 aspect-h-3"
+            class="aspect-w-2 aspect-h-3 hidden sm:block"
           />
           <div
             :style="`background: center / contain no-repeat url('./people/9.svg');`"
-            class="aspect-w-2 aspect-h-3"
+            class="aspect-w-2 aspect-h-3 hidden sm:block"
           />
         </div>
 
-        <div class="flex gap-6 flex-col lg:flex-row mb-6">
+        <div class="flex gap-6 flex-col md:flex-row mb-6">
           <div
-            class="bg-white bg-opacity-10 lg:w-4/6 rounded-2xl flex flex-col items-center justify-center px-12 h-72 gap-8"
+            class="bg-white bg-opacity-10 md:w-4/6 rounded-2xl flex flex-col items-center justify-center px-12 h-52 sm:h-72 gap-4 sm:gap-8"
           >
-            <div class="text-8xl font-bold">152</div>
+            <div class="text-6xl sm:text-8xl font-bold">152</div>
             <div class="text-xl">
               использовали для построения модели машинного обучения
             </div>
           </div>
           <div
-            class="bg-white bg-opacity-10 lg:w-2/6 rounded-2xl flex flex-col items-center justify-center px-12 h-72 gap-8"
+            class="bg-white bg-opacity-10 md:w-2/6 rounded-2xl flex flex-col items-center justify-center px-12 h-52 sm:h-72 gap-4 sm:gap-8"
           >
             <div
               :style="`background: center / contain no-repeat url('./people/5.svg');`"
-              class="aspect-w-1 aspect-h-1 h-44 w-full -scale-x-100"
+              class="aspect-w-1 aspect-h-1 h-32 w-32 md:h-44 md:w-44 -scale-x-100"
             />
           </div>
         </div>
 
-        <div class="flex gap-6 flex-col lg:flex-row">
+        <div class="flex gap-6 flex-col-reverse md:flex-row">
           <div
-            class="bg-white bg-opacity-10 lg:w-2/6 rounded-2xl flex flex-col items-center justify-center px-12 h-72 gap-8"
+            class="bg-white bg-opacity-10 md:w-2/6 rounded-2xl flex flex-col items-center justify-center px-12 h-52 sm:h-72 gap-4 sm:gap-8"
           >
-            <div
+          <div
               :style="`background: center / contain no-repeat url('./people/8.svg');`"
-              class="aspect-w-1 aspect-h-1 h-44 w-full"
+              class="aspect-w-1 aspect-h-1 h-32 w-32 md:h-44 md:w-44"
             />
           </div>
           <div
-            class="bg-white bg-opacity-10 lg:w-4/6 rounded-2xl flex flex-col items-center justify-center px-12 h-72 gap-8"
+            class="bg-white bg-opacity-10 md:w-4/6 rounded-2xl flex flex-col items-center justify-center px-12 h-52 sm:h-72 gap-4 sm:gap-8"
           >
-            <div class="text-8xl font-bold">150</div>
+            <div class="text-6xl sm:text-8xl font-bold">150</div>
             <div class="text-xl">факторов городской среды учтено в анализе</div>
           </div>
         </div>
       </div>
     </div>
 
-    <div class="flex flex-col gap-20 mx-auto max-w-screen-lg px-8 py-24">
+    <div class="flex flex-col gap-12 sm:gap-20 mx-auto max-w-screen-lg px-8 py-12 sm:py-24">
       <Content
         title="Изучите влияние факторов городской среды"
         description="Мы анализируем, как факторы городской среды влияют на здоровье населения, пссихологическую обстановку, уровень жизни"
@@ -172,7 +182,7 @@
     </div>
 
     <div class="bg-cyan-950">
-      <div class="flex flex-col mx-auto max-w-screen-lg px-8 py-28 text-white">
+      <div class="flex flex-col mx-auto max-w-screen-lg px-8 py-12 sm:py-24 text-white">
         <div class="flex gap-6 flex-col lg:flex-row mb-6">
           <Card
             title="Населенный пункт"
@@ -209,10 +219,10 @@
       </div>
     </div>
 
-    <div class="flex flex-col gap-12 mx-auto max-w-screen-lg px-8 py-20">
+    <div class="flex flex-col gap-6 sm:gap-12 mx-auto max-w-screen-lg px-8 py-12 sm:py-24">
       <div class="text-3xl text-red-600 uppercase font-bold">Немного о нас</div>
-      <div class="flex flex-col md:flex-row gap-20 font-light justify-between">
-        <div class="md:w-1/3">
+      <div class="flex flex-col sm:flex-row gap-12 md:gap-20 font-light justify-between">
+        <div class="w-full sm:w-1/3">
           <div class="text-red-600 mb-2">Почему мы этим занимаемся?</div>
           <div>
             Мы анализируем, как факторы городской среды влияют на здоровье
@@ -220,7 +230,7 @@
             уровня здоровья населения за счет качественного анализа
           </div>
         </div>
-        <div class="md:w-1/3">
+        <div class="w-full sm:w-1/3">
           <div class="text-red-600 mb-2">Как мы анализируем?</div>
           <div>
             Мы анализируем, как факторы городской среды влияют на здоровье
@@ -228,7 +238,7 @@
             уровня здоровья населения за счет качественного анализа
           </div>
         </div>
-        <div class="md:w-1/3">
+        <div class="w-full sm:w-1/3">
           <div class="text-red-600 mb-2">Какие технологии применяем?</div>
           <div>
             Мы анализируем, как факторы городской среды влияют на здоровье
@@ -238,11 +248,13 @@
         </div>
       </div>
     </div>
-    <div class="flex flex-col gap-8 items-center justify-center mx-auto max-w-screen-lg px-8 pt-8 pb-20">
+    <div
+      class="flex flex-col gap-8 items-center justify-center mx-auto max-w-screen-lg px-8 pt-8 pb-12 sm:pb-20"
+    >
       <div class="h-44">
         <div
-        :style="`background: center / contain no-repeat url('./logo_dark.svg');`"
-        class="aspect-w-1 aspect-h-1 h-44 w-44"
+          :style="`background: center / contain no-repeat url('./logo_dark.svg');`"
+          class="aspect-w-1 aspect-h-1 h-44 w-44"
         />
       </div>
     </div>
