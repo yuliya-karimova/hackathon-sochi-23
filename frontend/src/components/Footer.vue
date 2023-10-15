6e35e5b177541b06dc33250055e491990185f30f
Yuliya Karimova <yuliya.karimova@wn.media>
2023-10-15 10:38:47 +0300

    feat: update

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,12 +1,20 @@
 <template>
-     <div
-      class="flex items-center justify-center mx-auto bg-white px-8 pt-8 pb-12 sm:pb-20"
+  <div
+    class="flex flex-col gap-6 items-center justify-center mx-auto bg-white px-8 pt-8 pb-12 sm:pb-20"
+  >
+    <a
+      class="h-44"
+      href="https://github.com/yuliya-karimova/hackathon-sochi-23"
     >
-      <a class="h-44" href="https://github.com/yuliya-karimova/hackathon-sochi-23">
-        <div
-          :style="`background: center / contain no-repeat url('./logo_dark.svg');`"
-          class="aspect-w-1 aspect-h-1 h-44 w-44"
-        />
-      </a>
-    </div>
+      <div
+        :style="`background: center / contain no-repeat url('./logo_dark.svg');`"
+        class="aspect-w-1 aspect-h-1 h-44 w-44"
+      />
+    </a>
+    <a
+      class="text-cyan-900 hover:opacity-80 cursor-pointer"
+      href="https://github.com/yuliya-karimova/hackathon-sochi-23"
+      >GitHub @yuliya-karimova</a
+    >
+  </div>
 </template>
